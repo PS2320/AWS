9aa9d4b626db6432ee519ce894bf37e2b659c36d
PS2320 <151596510+PS2320@users.noreply.github.com>
2024-01-12 10:34:54 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,6 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull ps2320/simple-python-flask-app
-
+docker pull ps2320/simple-python-flask-app:latest
 # Run the Docker image as a container
-docker run -d -p 5000:5000 ps2320/simple-python-flask-app
+docker run -d -p 5000:5000 ps2320/simple-python-flask-app:latest
